00e051519e8943f7039d8ff93d1f4b4ef9cc5a96
Hunain Bin Sajid <Hunainbinsajid@gmail.com>
2024-05-06 18:37:24 +0500

    feat: add server endpoint widgets for devmode

--- my-app/src/components/home/server-info.tsx
+++ my-app/src/components/home/server-info.tsx
@@ -24,7 +24,7 @@ interface IServerInfo {
   loginResponse: string;
 }
 
-const ApiWidget = ({ severity = "error", title, children }) => {
+const ApiWidget = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <Box
@@ -37,7 +37,16 @@ const ApiWidget = ({ severity = "error", title, children }) => {
         borderRadius: "8px",
       }}
     >
-      <Box sx={{ display: "flex", columnGap: "8px", flexDirection: "row" }}>
+      <Box
+        sx={{
+          display: "flex",
+          columnGap: "8px",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {title}
+
         <IconButton
           aria-label="expand row"
           size="small"
@@ -45,7 +54,6 @@ const ApiWidget = ({ severity = "error", title, children }) => {
         >
           {isOpen ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
         </IconButton>
-        {title}
       </Box>
       {children && (
         <Collapse in={isOpen}>
@@ -69,6 +77,21 @@ const ServerInfo: React.FC<IServerInfo> = ({
   handleLogin,
   loginRequest,
   loginResponse,
+  verifyUrl,
+  setVerifyUrl,
+  verifyRequest,
+  verifyResponse,
+  handleVerify,
+  statusUrl,
+  setStatusUrl,
+  statusRequest,
+  statusResponse,
+  handleStatus,
+  reportUrl,
+  setReportUrl,
+  reportRequest,
+  reportResponse,
+  handleReportUpload,
 }) => {
   return (
     <Grid container spacing={1}>
@@ -98,18 +121,21 @@ const ServerInfo: React.FC<IServerInfo> = ({
           title={
             <Typography variant="h6">
               <strong>Ping</strong>{" "}
+              <Typography variant="caption">Nudges server</Typography>
             </Typography>
           }
-          severity="info"
         >
           <Grid container spacing={1}>
             <Grid item xs={12}>
-              <Box>
+              <Box
+                sx={{ display: "flex", flexDirection: "row", columnGap: "8px" }}
+              >
                 <TextField
                   id="outlined-basic"
                   label="Ping server URl"
                   variant="outlined"
                   fullWidth
+                  prefix="https://"
                   size="small"
                   value={pingUrl}
                   onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
@@ -127,15 +153,91 @@ const ServerInfo: React.FC<IServerInfo> = ({
                   </Button>
                 </Box>
               </Box>
-              <Box>
-                <textarea
-                  id="message"
-                  rows={12}
-                  className="textarea-data"
-                  placeholder={"ping response"}
-                  defaultValue={JSON.stringify(pingResponse, null, 2)}
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Response:</strong>
+              </Typography>
+              <textarea
+                id="message"
+                rows={12}
+                cols={120}
+                className="textarea-data"
+                placeholder={"ping response"}
+                value={JSON.stringify(pingResponse, null, 2)}
+              />
+            </Grid>
+          </Grid>
+        </ApiWidget>
+      </Grid>
+      <Grid item xs={12}>
+        <br />
+      </Grid>
+      <Grid item xs={12} lg={12}>
+        <ApiWidget
+          title={
+            <Typography variant="h6">
+              <strong>Verify</strong>{" "}
+              <Typography variant="caption">
+                Returns if the headers are properly signed
+              </Typography>
+            </Typography>
+          }
+        >
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Box
+                sx={{ display: "flex", flexDirection: "row", columnGap: "8px" }}
+              >
+                <TextField
+                  id="outlined-basic"
+                  label="Verify URl"
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  value={verifyUrl}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setVerifyUrl(event.target.value);
+                  }}
                 />
+                <Box>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    disabled={false}
+                    onClick={handleVerify}
+                  >
+                    Verify
+                  </Button>
+                </Box>
               </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Request:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-post"
+                rows={12}
+                cols={120}
+                placeholder={"login post"}
+                value={JSON.stringify(verifyRequest, null, 2)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Response:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-response"
+                rows={12}
+                cols={120}
+                placeholder={"login response"}
+                value={JSON.stringify(verifyResponse, null, 2)}
+              />
             </Grid>
           </Grid>
         </ApiWidget>
@@ -148,54 +250,212 @@ const ServerInfo: React.FC<IServerInfo> = ({
           title={
             <Typography variant="h6">
               <strong>Login</strong>{" "}
+              <Typography variant="caption">
+                Given an AID and vLEI, returns information about the login
+              </Typography>
             </Typography>
           }
-          severity="info"
         >
-          <Box sx={{ display: "flex", columnGap: "8px" }}>
-            <TextField
-              id="outlined-basic"
-              label="Login server URl"
-              variant="outlined"
-              fullWidth
-              size="small"
-              value={loginUrl}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                setLoginUrl(event.target.value);
-              }}
-            />
-            <Box>
-              <Button
-                variant="contained"
-                color="primary"
-                size="small"
-                disabled={false}
-                onClick={handleLogin}
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Box
+                sx={{ display: "flex", flexDirection: "row", columnGap: "8px" }}
               >
-                Verify
-              </Button>
-            </Box>
-          </Box>
-          <Typography variant="body2">
-            <strong>Request:</strong>
-          </Typography>
-          <textarea
-            className="textarea-data"
-            id="login-post"
-            rows={12}
-            placeholder={"login post"}
-            value={JSON.stringify(loginRequest, null, 2)}
-          />
-          <Typography variant="body2">
-            <strong>Response:</strong>
-          </Typography>
-          <textarea
-            className="textarea-data"
-            id="login-response"
-            rows={12}
-            placeholder={"login response"}
-            value={JSON.stringify(loginResponse, null, 2)}
-          />
+                <TextField
+                  id="outlined-basic"
+                  label="Login server URl"
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  value={loginUrl}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setLoginUrl(event.target.value);
+                  }}
+                />
+                <Box>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    disabled={false}
+                    onClick={handleLogin}
+                  >
+                    Login
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Request:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-post"
+                rows={12}
+                cols={120}
+                placeholder={"login post"}
+                value={JSON.stringify(loginRequest, null, 2)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Response:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-response"
+                rows={12}
+                cols={120}
+                placeholder={"login response"}
+                value={JSON.stringify(loginResponse, null, 2)}
+              />
+            </Grid>
+          </Grid>
+        </ApiWidget>
+      </Grid>
+      <Grid item xs={12}>
+        <br />
+      </Grid>
+      <Grid item xs={12} lg={12}>
+        <ApiWidget
+          title={
+            <Typography variant="h6">
+              <strong>Status</strong>{" "}
+              <Typography variant="caption">
+                Given an AID returns information about the upload status
+              </Typography>
+            </Typography>
+          }
+        >
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Box
+                sx={{ display: "flex", flexDirection: "row", columnGap: "8px" }}
+              >
+                <TextField
+                  id="outlined-basic"
+                  label="Login server URl"
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  value={statusUrl}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setStatusUrl(event.target.value);
+                  }}
+                />
+                <Box>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    disabled={false}
+                    onClick={handleStatus}
+                  >
+                    Send
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Request:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-post"
+                rows={12}
+                cols={120}
+                placeholder={"login post"}
+                value={JSON.stringify(statusRequest, null, 2)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Response:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-response"
+                rows={12}
+                cols={120}
+                placeholder={"login response"}
+                value={JSON.stringify(statusResponse, null, 2)}
+              />
+            </Grid>
+          </Grid>
+        </ApiWidget>
+      </Grid>
+      <Grid item xs={12}>
+        <br />
+      </Grid>
+      <Grid item xs={12} lg={12}>
+        <ApiWidget
+          title={
+            <Typography variant="h6">
+              <strong>Report</strong>{" "}
+              <Typography variant="caption">
+                Given an AID and DIG, returns information about the upload
+              </Typography>
+            </Typography>
+          }
+        >
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Box
+                sx={{ display: "flex", flexDirection: "row", columnGap: "8px" }}
+              >
+                <TextField
+                  id="outlined-basic"
+                  label="Report upload server URl"
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  value={reportUrl}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setReportUrl(event.target.value);
+                  }}
+                />
+                <Box>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    disabled={false}
+                    onClick={handleReportUpload}
+                  >
+                    Send
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Request:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-post"
+                rows={12}
+                cols={120}
+                placeholder={"login post"}
+                value={JSON.stringify(reportRequest, null, 2)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">
+                <strong>Response:</strong>
+              </Typography>
+              <textarea
+                className="textarea-data"
+                id="login-response"
+                rows={12}
+                cols={120}
+                placeholder={"login response"}
+                value={JSON.stringify(reportResponse, null, 2)}
+              />
+            </Grid>
+          </Grid>
         </ApiWidget>
       </Grid>
     </Grid>
