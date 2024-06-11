bf6fe73f1302d8ee09386addb2fae25c4871a1bd
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2024-06-11 22:00:20 +0500

    feat: replace cra react-scripts with vite (#50)

---
+++ my-app/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
+  build: {
+    outDir: "build",
+  },
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
